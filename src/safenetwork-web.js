ff87abee9c96f8e2871c650930600fa23497298b
theWebalyst <github@happybeing.com>
2018-06-09 13:38:59 +0100

    Updated file comments and TODOs

--- src/safenetwork-web.js
+++ src/safenetwork-web.js
@@ -1,6 +1,10 @@
 /*
  * SAFEnetwork WEB API - RESTful web services on top of SAFE FS API
  *
+ * - provides a safe: fetch (using proto-fetch) which redirects to safenetworkjs webFetch() so a web application can access SAFE as a RESTful web client
+ * - builds a bundle suitable for a Web application
+ *
+ * See: http://docs.maidsafe.net/safe_app_nodejs/#safeappwebfetch
 
 TODO theWebalyst:
   see safenetwork-api.js for TODOs!
